618d3fc17972ce2996bb632bbdb661498efdedcd
Lita Beach <litabeach123@gmail.com>
2021-03-15 19:28:50 -0500

    all get, post, put and delete routes are working

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Category.create(req.body);
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
@@ -49,7 +49,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Category.update(req.body,
+    const tagData = await Tag.update(req.body,
       {
         where: {
           id: req.params.id,
@@ -62,10 +62,10 @@ router.put('/:id', async (req, res) => {
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Category.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       },
@@ -75,7 +75,7 @@ router.delete('/:id', (req, res) => {
       res.status(404).json({ message: 'No tag found with that id!' });
       return;
     }
-    
+
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
